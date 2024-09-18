c41641475e9bdf703d10aca0e9745f8ba13541be
cracycot <cracycot@mail.ru>
2024-09-18 15:57:00 +0300

    fix: made BasketUpdateRequest record

--- src/main/java/org/example/machine_service/DTO/BasketUpdateRequest.java
+++ src/main/java/org/example/machine_service/DTO/BasketUpdateRequest.java
@@ -1,22 +1,4 @@
 package org.example.machine_service.DTO;
 
-public class BasketUpdateRequest {
-    private String productName;
-    private int page;
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
+public record BasketUpdateRequest(String productName, int page) {
 }

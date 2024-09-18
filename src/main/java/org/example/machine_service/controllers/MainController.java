eeb85d2c52260ef2ac9b5f60f216911a84f95855
cracycot <cracycot@mail.ru>
2024-09-18 14:03:06 +0300

    fix: made segregation

--- src/main/java/org/example/machine_service/controllers/MainController.java
+++ src/main/java/org/example/machine_service/controllers/MainController.java
@@ -42,75 +42,6 @@ public class MainController {
         return "Main";
     }
 
-    @PostMapping("/add-to-basket")
-    public ResponseEntity<?> addToBasket(@RequestParam Long productId, HttpSession session) throws ProductNotFindException {
-        // Попытаться получить корзину из сессии.
-        HashMap<String, List<Object>> basket =
-                (HashMap<String, List<Object>>) session.getAttribute("basket");
-
-        if (basket == null) {
-            basket = new HashMap<>();
-            session.setAttribute("basket", basket);
-        }
-
-        // Извлечь продукт, используя предоставленный ID.
-        Product product = productService.get_product(productId); // Реализовать этот метод.
-
-        if (product != null) {
-            String productName = product.getName();
-            // Проверить, есть ли продукт уже в корзине
-            if (basket.containsKey(productName) && ((Integer) (basket.get(productName)).get(0) < (Integer) (basket.get(productName)).get(4))) {
-                // Обновить существующее количество продукта
-                List<Object> existingEntry = basket.get(productName);
-                existingEntry.set(0, (Integer) existingEntry.get(0) + 1); // Увеличить количество на 1
-            } else if (!basket.containsKey(productName)) {
-                // Продукт отсутствует в корзине, добавить новую запись
-                List<Object> productDetails = Arrays.asList(
-                        1, // Количество
-                        product.getCategory(),
-                        product.getArticle(),
-                        product.getPrice(),
-                        product.getInStock()
-                );
-                basket.put(productName, productDetails);
-            }
-        }
-        // Вернуть успешный ответ.
-        return ResponseEntity.ok().body(Collections.singletonMap("result", "success"));
-    }
-
-    @PostMapping("/basket/increase")
-    public ResponseEntity<?> increaseQuantity(@RequestParam String productName, HttpSession session) {
-        return updateQuantity(session, productName, 1);
-    }
-
-    @PostMapping("/basket/decrease")
-    public ResponseEntity<?> decreaseQuantity(@RequestParam String productName, HttpSession session) {
-        return updateQuantity(session, productName, -1);
-    }
-
-    private synchronized ResponseEntity<?> updateQuantity(HttpSession session, String productName, int change) {
-        HashMap<String, List<Object>> basket = (HashMap<String, List<Object>>) session.getAttribute("basket");
-        if (basket != null && basket.containsKey(productName)) {
-            List<Object> productDetails = basket.get(productName);
-            int currentQuantity = (Integer) productDetails.get(0);
-            int inStock = (Integer) productDetails.get(4);
-
-            int newQuantity = currentQuantity + change;
-
-            if (newQuantity > 0 && newQuantity <= inStock) {
-                productDetails.set(0, newQuantity);
-            } else if (newQuantity <= 0) {
-                basket.remove(productName);
-            } else {
-                return ResponseEntity.badRequest().body("Невозможно обновить количество товара, превышено количество на складе.");
-            }
-
-            session.setAttribute("basket", basket);
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
-    }
 
     @GetMapping("/Bid")
     public String BidPage(Model model) {
@@ -159,16 +90,6 @@ public class MainController {
         }
     }
 
-    //    @PostMapping("/SendOrder")
-//    public ResponseEntity<Object> sendOrder(@RequestBody OrderRequest orderRequest) {
-//        HashMap<String, List<Object>> basket = orderRequest.getBasket();
-//        String contact = orderRequest.getContact();
-//
-//        if (!(basket.isEmpty() || contact.isEmpty())) {
-//            emailService.sendOrder(basket, contact);
-//        }
-//        return ResponseEntity.ok().body("Письмо отправлено");
-//    }
     @PostMapping("/SendOrder")
     public ResponseEntity<Object> sendOrder(@Valid @RequestBody OrderRequest orderRequest) {
         HashMap<String, List<Object>> basket = orderRequest.getBasket();
@@ -185,11 +106,6 @@ public class MainController {
         }
     }
 
-    @GetMapping("/CleanBasket")
-    public ResponseEntity<?> cleanBasket(HttpSession session) {
-        session.setAttribute("basket", new HashMap<String, List<Object>>());
-        return ResponseEntity.ok().build();
-    }
 
     // Внутренний класс для десериализации
     private static class OrderRequest {
@@ -223,9 +139,7 @@ public class MainController {
         Page<Product> elements;
         elements = productService.searchAllProducts(pageable);
         if (search == null) {
-            System.out.println(2);
         } else {
-            System.out.println("1");
 //            elements = productService.searchProduct(search, pageable);
         }
         model.addAttribute("title", "Каталог");
@@ -237,55 +151,7 @@ public class MainController {
         return "Catalog";
     }
 
-    @GetMapping("/Basket")
-    public String basket(Model model, HttpSession session) {
-        HashMap<String, List<Object>> basket = (HashMap<String, List<Object>>) session.getAttribute("basket");
 
-        // If the basket is null, initialize a new one and add it to the session.
-        if (basket == null) {
-            basket = new HashMap<String, List<Object>>();
-
-            session.setAttribute("basket", basket);
-        }
-        model.addAttribute("items", basket);
-        return "Basket";
-    }
-    @GetMapping("/basket/products")
-    public ResponseEntity<Map<String, Object>> getBasketProducts(
-            HttpSession session,
-            @RequestParam(value = "page", defaultValue = "1") int page,
-            @RequestParam(value = "size", defaultValue = "5") int size) {
-
-        HashMap<String, List<Object>> basket = (HashMap<String, List<Object>>) session.getAttribute("basket");
-        if (basket == null) {
-            basket = new HashMap<>();
-        }
-
-        List<Map<String, Object>> itemsList = new ArrayList<>();
-        for (Map.Entry<String, List<Object>> entry : basket.entrySet()) {
-            Map<String, Object> itemMap = new HashMap<>();
-            itemMap.put("name", entry.getKey());
-            itemMap.put("quantity", entry.getValue().get(0));
-            itemMap.put("article", entry.getValue().get(2));
-            itemMap.put("price", entry.getValue().get(3));
-            itemMap.put("inStock", entry.getValue().get(4));
-            itemsList.add(itemMap);
-        }
-
-        int totalItems = itemsList.size();
-        int totalPages = (int) Math.ceil((double) totalItems / size);
-
-        int start = (page - 1) * size;
-        int end = Math.min(start + size, totalItems);
-        List<Map<String, Object>> pageItems = itemsList.subList(start, end);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("items", pageItems);
-        response.put("totalPages", totalPages);
-        response.put("currentPage", page);
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
 
     @GetMapping("/MainPage")
     public String MainPage(Model model) {

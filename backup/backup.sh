b67317a27506e03edecd2bdf1e1f8c7a742f6643
cracycot <cracycot@mail.ru>
2024-09-23 15:12:42 +0300

    fix: updates bckup scripts to work once in the week

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,37 +1,3 @@
-##!/bin/bash
-#
-## Директория для бэкапов
-#BACKUP_DIR="/backup"
-## Имя бэкапа с текущей датой
-#BACKUP_NAME="backup_$(date +%Y%m%d_%H%M%S).sql"
-## Полный путь до файла бэкапа
-#BACKUP_FILE="${BACKUP_DIR}/${BACKUP_NAME}"
-#
-## Параметры подключения к БД
-#DB_USER="kirilllesniak"
-#DB_PASSWORD="admin12345"
-#DB_NAME="machine_service"
-#DB_HOST="db"
-#
-## Экспорт пароля для pg_dump
-#export PGPASSWORD="${DB_PASSWORD}"
-#
-## Создать бэкап и записывать ошибки в лог
-#pg_dump -U "${DB_USER}" -h "${DB_HOST}" -d "${DB_NAME}" > "${BACKUP_FILE}" 2> /backup/backup_error.log
-#
-## Проверить, выполнена ли команда pg_dump успешно
-#if [ $? -ne 0 ]; then
-#  echo "Ошибка при создании бэкапа: $(cat /backup/backup_error.log)" >&2
-#fi
-#
-## Удалить старые бэкапы, если их больше 4
-#BACKUP_COUNT=$(ls -1 ${BACKUP_DIR}/*.sql 2>/dev/null | wc -l)
-#if [ ${BACKUP_COUNT} -gt 4 ]; then
-#  # Удалить самый старый файл
-#  OLDEST_FILE=$(ls -1t ${BACKUP_DIR}/*.sql | tail -n 1)
-#  rm -f "${OLDEST_FILE}"
-#fi
-#!/bin/bash
 #!/bin/bash
 # Директория для бэкапов на вашей машине
 BACKUP_DIR="/backup"

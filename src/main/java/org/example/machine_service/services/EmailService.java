0e2bbb3bc8a58df267c3b15423b2e9f011326cf8
cracycot <cracycot@mail.ru>
2024-09-23 19:50:42 +0300

    upd: password

--- src/main/java/org/example/machine_service/services/EmailService.java
+++ src/main/java/org/example/machine_service/services/EmailService.java
@@ -3,6 +3,7 @@ package org.example.machine_service.services;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -15,6 +16,8 @@ import static java.awt.SystemColor.text;
 
 @Service
 public class EmailService {
+    @Value("${MAIL_ADDRESS}")
+    private String mailAddress;
 
     @Autowired
     private JavaMailSender mailSender;

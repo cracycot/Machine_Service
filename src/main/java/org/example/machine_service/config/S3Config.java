265bef2c94c0b9b926cf2f29d266a26f2b243c43
cracycot <cracycot@mail.ru>
2024-10-07 12:00:59 +0300

    feat: made S3 config

---
+++ src/main/java/org/example/machine_service/config/S3Config.java
@@ -0,0 +1,38 @@
+package org.example.machine_service.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${s3.accesskey}")
+    private String accessKey;
+    @Value("${s3.secretkey}")
+    private String secretKey;
+    private String endpointUrl = "https://s3.selcdn.ru"; // Selectel S3 endpoint
+
+    @Bean
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        clientConfig.setProtocol(Protocol.HTTPS);
+
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(
+                        new AmazonS3ClientBuilder.EndpointConfiguration(endpointUrl, "ru-1")
+                )
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfig)
+                .build();
+    }
+}

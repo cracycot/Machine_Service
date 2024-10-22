391a001f1666d0da0f7eb7ba801bea62b1817838
cracycot <cracycot@mail.ru>
2024-10-22 23:13:50 +0300

    fix: uploading photos

--- src/main/resources/static/js/uploadPhotos.js
+++ src/main/resources/static/js/uploadPhotos.js
@@ -1,111 +1,63 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const productForm = document.getElementById('productForm');
-//     if (productForm) {
-//         productForm.addEventListener('submit', async function(event) {
-//             event.preventDefault();
-//
-//             const form = event.target;
-//             const formData = new FormData(form);
-//
-//             // Получаем все файлы изображений
-//             const files = document.getElementById('images').files;
-//             const imageUrls = [];
-//
-//             // Загружаем изображения на сервер
-//             for (const file of files) {
-//                 const imageFormData = new FormData();
-//                 imageFormData.append('file', file);
-//                 imageFormData.append('fileName', file.name);
-//
-//                 try {
-//                     const response = await fetch('/upload', {
-//                         method: 'POST',
-//                         body: imageFormData
-//                     });
-//
-//                     if (response.ok) {
-//                         const data = await response.json();
-//                         console.log("фото отправлено")
-//                         imageUrls.push(data.url); // Добавляем полученный URL в список
-//                     } else {
-//                         console.error('Ошибка загрузки изображения', response.statusText);
-//                     }
-//                 } catch (error) {
-//                     console.error('Ошибка загрузки изображения', error);
-//                 }
-//             }
-//
-//             // Добавляем URL-адреса изображений в данные продукта
-//             formData.append('imageUrls', JSON.stringify(imageUrls));
-//
-//             // Отправляем данные продукта на сервер
-//             try {
-//                 const response = await fetch('/create', {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                     },
-//                     body: JSON.stringify(Object.fromEntries(formData))
-//                 });
-//
-//                 if (response.ok) {
-//                     alert('Продукт успешно добавлен');
-//                     window.location.reload();
-//                 } else {
-//                     console.error('Ошибка создания продукта', response.statusText);
-//                 }
-//             } catch (error) {
-//                 console.error('Ошибка создания продукта', error);
-//             }
-//         });
-//     } else {
-//         console.error('Форма с id="productForm" не найдена');
-//     }
-// });
-// document.addEventListener('DOMContentLoaded', function () {
-//     const productForm = document.getElementById('productForm');
-//     if (productForm) {
-//         productForm.addEventListener('submit', async function(event) {
-//             event.preventDefault();
-//
-//             const form = event.target;
-//             const formData = new FormData(form);
-//
-//             // Получаем все файлы изображений
-//             const files = document.getElementById('images').files;
-//
-//             // Добавляем все файлы в formData под одним именем 'files'
-//             console.log(files.length);
-//             for (let i = 0; i < files.length; i++) {
-//                 formData.append('files', files[i]);
-//             }
-//
-//             // Отправляем данные продукта и файлы на сервер
-//             try {
-//                 const response = await fetch('/product/create', {
-//                     method: 'POST',
-//                     body: formData
-//                 });
-//
-//                 if (response.ok) {
-//                     alert('Продукт успешно добавлен');
-//                     window.location.reload();
-//                 } else {
-//                     console.error('Ошибка создания продукта', response.statusText);
-//                 }
-//             } catch (error) {
-//                 console.error('Ошибка создания продукта', error);
-//             }
-//         });
-//     } else {
-//         console.error('Форма с id="productForm" не найдена');
-//     }
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     const productForm = document.getElementById('productForm');
     const addImageButton = document.getElementById('addImageButton');
     const fileInputsContainer = document.getElementById('fileInputsContainer');
+
+    if (addImageButton) {
+        addImageButton.addEventListener('click', function() {
+            const newFileInputWrapper = document.createElement('div');
+            newFileInputWrapper.className = 'file-input-wrapper';
+
+            const newFileInput = document.createElement('input');
+            newFileInput.type = 'file';
+            newFileInput.name = 'images';
+            newFileInput.className = 'image-input';
+            newFileInput.accept = 'image/*';
+            newFileInput.style.display = 'none'; // Поле загрузки файла скрыто
+            newFileInputWrapper.appendChild(newFileInput);
+
+            const customButton = document.createElement('button');
+            customButton.type = 'button';
+            customButton.textContent = 'Выбрать фото';
+            customButton.className = 'custom-select-button';
+            customButton.onclick = function() {
+                newFileInput.click(); // Открываем диалог выбора файла
+            };
+            newFileInputWrapper.appendChild(customButton);
+
+            const fileNameLabel = document.createElement('span');
+            fileNameLabel.textContent = 'Файл не выбран';
+            fileNameLabel.className = 'file-name-label';
+            newFileInputWrapper.appendChild(fileNameLabel);
+
+            // Обработчик изменения для отображения имени файла
+            newFileInput.addEventListener('change', function() {
+                if (newFileInput.files.length > 0) {
+                    fileNameLabel.textContent = newFileInput.files[0].name;
+
+                    const removeButton = document.createElement('button');
+                    removeButton.type = 'button';
+                    removeButton.className = 'remove-image-button';
+                    removeButton.textContent = 'Удалить';
+                    removeButton.onclick = function() {
+                        removeImageInput(newFileInputWrapper);
+                    };
+
+                    // Добавляем кнопку удаления справа от имени файла
+                    if (!newFileInputWrapper.contains(removeButton)) {
+                        fileNameLabel.insertAdjacentElement('afterend', removeButton);
+                    }
+                }
+            });
+
+            fileInputsContainer.appendChild(newFileInputWrapper);
+        });
+    }
+
+    // Функция для удаления выбранного файла
+    window.removeImageInput = function (wrapper) {
+        wrapper.remove();
+    };
 
     if (addImageButton) {
         addImageButton.addEventListener('click', function() {
@@ -127,20 +79,42 @@ document.addEventListener('DOMContentLoaded', function () {
             // Собираем все данные из формы, кроме файлов
             const inputs = productForm.querySelectorAll('input[name]:not([type="file"]), select[name]');
             inputs.forEach(input => {
-                formData.append(input.name, input.value);
+                if (input.name === 'condition') {
+                    formData.append(input.name, input.value === 'newCondition');
+                } else {
+                    formData.append(input.name, input.value);
+                }
             });
 
             // Собираем все файлы
             const fileInputs = productForm.querySelectorAll('.image-input');
+            let fileUploaded = false;
             fileInputs.forEach(fileInput => {
                 if (fileInput.files[0]) {
-                    console.log(fileInput.files[0])
                     formData.append('files', fileInput.files[0]);
+                    console.log(fileInput.files[0], fileInput.files)
+                    fileUploaded = true;
                 }
             });
+            // Если не было загружено ни одного файла, добавляем дефолтное изображение
+            if (!fileUploaded) {
+                try {
+                    const defaultImageResponse = await fetch('/img/connection.jpg');
+                    const defaultImageBlob = await defaultImageResponse.blob();
+                    const defaultFile = new File([defaultImageBlob], 'connection.jpg', { type: defaultImageBlob.type });
+                    formData.append('files', defaultFile); // Просто добавляем defaultFile без попытки обращаться к files[0]
+                    console.log(defaultFile); // Логируем объект файла
+                } catch (error) {
+                    console.error('Ошибка при загрузке дефолтного изображения', error);
+                    alert('Не удалось загрузить дефолтное изображение');
+                    return;
+                }
+            }
+            const loadingSpinner = document.getElementById('loadingSpinner');
 
             // Отправляем данные продукта и файлы на сервер
             try {
+                loadingSpinner.style.display = 'block';
                 const response = await fetch('/product/create', {
                     method: 'POST',
                     body: formData
@@ -156,6 +130,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             } catch (error) {
                 console.error('Ошибка создания продукта', error);
+            } finally {
+                loadingSpinner.style.display = 'none';
             }
         });
     } else {
